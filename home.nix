6c94ead2c102c7d57f6ab283f70b5707fbfd783c
Gibson Berg <berg.gibson@gmail.com>
2025-05-30 22:57:12 -0500

    Added basic home software

--- home.nix
+++ home.nix
@@ -66,6 +66,7 @@
   # };
 
   # Packages that should be installed to the user profile.
+  # TODO: Separate out into separate file, break down by host?
   home.packages = with pkgs; [
 
     # GNOME
@@ -75,71 +76,31 @@
     gnomeExtensions.dash-to-dock
     # gnome-tweaks
 
-    # here is some command line tools I use frequently
-    # feel free to add your own or remove some of them
+    # Graphical Tools
+    alpaca
+    amberol
+    celluloid
+    dialect
+    gimp3
+    obsidian
+    onlyoffice-desktopeditors
+    resources
 
-   # neofetch
-   # nnn # terminal file manager
 
-    # archives
-   # zip
-   # xz
-   # unzip
-   # p7zip
-
-    # utils
+    # Command Line Tools
+    fastfetch
+    btop # htop replacement
+    iotop # io monitoring
+    iftop # network monitoring
    # ripgrep # recursively searches directories for a regex pattern
-   # jq # A lightweight and flexible command-line JSON processor
-   # yq-go # yaml processor https://github.com/mikefarah/yq
-   # eza # A modern replacement for ‘ls’
-   # fzf # A command-line fuzzy finder
-
-    # networking tools
-   # mtr # A network diagnostic tool
-   # iperf3
-   # dnsutils  # `dig` + `nslookup`
-   # ldns # replacement of `dig`, it provide the command `drill`
-   # aria2 # A lightweight multi-protocol & multi-source command-line download utility
-   # socat # replacement of openbsd-netcat
    # nmap # A utility for network discovery and security auditing
-   # ipcalc  # it is a calculator for the IPv4/v6 addresses
+
+    # Productivity
+    hugo # static site generator
 
     # misc
     cowsay
-   # file
-   # which
-   # tree
-   # gnused
-   # gnutar
-   # gawk
-   # zstd
-   # gnupg
 
-    # nix related
-    #
-    # it provides the command `nom` works just like `nix`
-    # with more details log output
-   # nix-output-monitor
-
-    # productivity
-   # hugo # static site generator
-   # glow # markdown previewer in terminal
-
-    btop  # replacement of htop/nmon
-    iotop # io monitoring
-    iftop # network monitoring
-
-    # system call monitoring
-   # strace # system call monitoring
-   # ltrace # library call monitoring
-   # lsof # list open files
-
-    # system tools
-   # sysstat
-   # lm_sensors # for `sensors` command
-   # ethtool
-   # pciutils # lspci
-   # usbutils # lsusb
   ];
 
   # basic configuration of git 
