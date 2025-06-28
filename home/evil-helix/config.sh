e2e1fd03007a7e20b11b38d194cd869a2889b028
Gibson Berg <berg.gibson@gmail.com>
2025-06-28 14:02:00 -0500

    Added slightly janky not-fully-declarative evil-helix config

---
+++ home/evil-helix/config.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# I have no idea how to declaratively configure evil-helix
+# so just run this to symlink the config
+ln -s ~/nixos-config/home/evil-helix/config.toml ~/.config/helix/config.toml
