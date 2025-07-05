a46ebf76b4289e66819197321a6632f229a6aff4
Gibson Berg <berg.gibson@gmail.com>
2025-07-05 18:07:20 -0500

    Temporarily(?) replaced Alpaca with straight ollama ROCM

--- hosts/hiawatha/programs.nix
+++ hosts/hiawatha/programs.nix
@@ -15,7 +15,7 @@
   environment.systemPackages = with pkgs; [
     wl-clipboard
 
-    alpaca # local LLM client
+    # alpaca # local LLM client
     solaar # Logitech unifying receiver client
 
     # Zen Browser
@@ -23,11 +23,21 @@
     inputs.zen-browser.packages.${pkgs.system}.default
   ];
 
-  # Hiawatha-specific package overrides
-  nixpkgs.config.packageOverrides = pkgs: {
-    # Set up ROCM for ollama
-    alpaca = pkgs.alpaca.override { ollama = pkgs.ollama-rocm; };
+  ### Temporarily(?) replacing Alpaca with straight Ollama --
+  ### The current Alpaca version in 25.05 seems to not have functioning ROCM
+  ### + doesn't "ollama stop" models upon closing for dedicated Ollama instances.
+  # # Hiawatha-specific package overrides
+  # nixpkgs.config.packageOverrides = pkgs: {
+  #   # Set up ROCM for ollama
+  #   alpaca = pkgs.alpaca.override { ollama = pkgs.ollama-rocm; };
+  # };
+
+  # Ollama: local LLM enabled as systemd service. Access with "ollama"
+  services.ollama = {
+    enable = true;
+    acceleration = "rocm";
   };
 
   hardware.logitech.wireless.enable = true; # Necessary for solaar
+
 }
